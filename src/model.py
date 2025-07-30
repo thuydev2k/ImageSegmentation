a9abb9bad1ebadf84167c50b5109e9ee023fc376
Thuy Nguyen <thuydev2k@gmail.com>
2025-07-30 19:06:06 +0900

    apply U-net model for segmentation.

--- src/model.py
+++ src/model.py
@@ -1,29 +1,90 @@
+import torch
 from torch import nn
+import torchvision.transforms.functional as TF
 
 import segmentation_models_pytorch as smp
 from segmentation_models_pytorch.losses import DiceLoss
 
 from config import ENCODER, WEIGHTS
 
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class DownBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DownBlock, self).__init__()
+        self.double_conv = DoubleConv(in_channels, out_channels)
+        self.down_sample = nn.MaxPool2d(2)
+
+    def forward(self, x):
+        skip_out = self.double_conv(x)
+        down_out = self.down_sample(skip_out)
+        return (down_out, skip_out)
+
+
+class UpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, up_sample_mode):
+        super(UpBlock, self).__init__()
+        if up_sample_mode == 'conv_transpose':
+            self.up_sample = nn.ConvTranspose2d(in_channels-out_channels, in_channels-out_channels, kernel_size=2, stride=2)
+        elif up_sample_mode == 'bilinear':
+            self.up_sample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            raise ValueError("Unsupported `up_sample_mode` (can take one of `conv_transpose` or `bilinear`)")
+        self.double_conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, down_input, skip_input):
+        x = self.up_sample(down_input)
+        x = torch.cat([x, skip_input], dim=1)
+        return self.double_conv(x)
+
 class SegmentationModel(nn.Module):
+    def __init__(self, out_classes=1, up_sample_mode='conv_transpose'):
+        super(SegmentationModel, self).__init__()
+        self.up_sample_mode = up_sample_mode
+        # Downsampling Path
+        self.down_conv1 = DownBlock(3, 64)
+        self.down_conv2 = DownBlock(64, 128)
+        self.down_conv3 = DownBlock(128, 256)
+        self.down_conv4 = DownBlock(256, 512)
+        # Bottleneck
+        self.double_conv = DoubleConv(512, 1024)
+        # Upsampling Path
+        self.up_conv4 = UpBlock(512 + 1024, 512, self.up_sample_mode)
+        self.up_conv3 = UpBlock(256 + 512, 256, self.up_sample_mode)
+        self.up_conv2 = UpBlock(128 + 256, 128, self.up_sample_mode)
+        self.up_conv1 = UpBlock(128 + 64, 64, self.up_sample_mode)
+        # Final Convolution
+        self.conv_last = nn.Conv2d(64, out_classes, kernel_size=1)
 
-  def __init__(self):
-    super(SegmentationModel, self).__init__()
+    def forward(self, x, masks = None):
+        x1, skip1_out = self.down_conv1(x)
+        x2, skip2_out = self.down_conv2(x1)
+        x3, skip3_out = self.down_conv3(x2)
+        x4, skip4_out = self.down_conv4(x3)
+        x5 = self.double_conv(x4)
+        x = self.up_conv4(x5, skip4_out)
+        x = self.up_conv3(x, skip3_out)
+        x = self.up_conv2(x, skip2_out)
+        x = self.up_conv1(x, skip1_out)
+        logits = self.conv_last(x)
 
-    self.arc = smp.Unet(
-        encoder_name = ENCODER,
-        encoder_weights = WEIGHTS,
-        in_channels = 3,
-        classes = 1,
-        activation = None
-    )
+        if masks != None:
+          loss1 = DiceLoss(mode = 'binary')(logits, masks)
+          loss2 = nn.BCEWithLogitsLoss()(logits, masks)
+          return logits, loss1 + loss2
 
-  def forward(self, images, masks = None):
-    logits = self.arc(images)
-
-    if masks != None:
-      loss1 = DiceLoss(mode = 'binary')(logits, masks)
-      loss2 = nn.BCEWithLogitsLoss()(logits, masks)
-      return logits, loss1 + loss2
-
-    return logits
+        return logits
